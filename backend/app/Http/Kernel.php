4495057a4a9ab52cf09d6603f9a0f664d7403b27
root <root@nc-ph-4747.mysol360.com>
2025-12-10 11:35:29 +0000

    Add audit logging middleware

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -54,6 +54,7 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\LogActivity::class,
         ],
 
     ];
